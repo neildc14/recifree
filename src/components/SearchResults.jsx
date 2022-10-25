10e9e3c0927f6958d630f453525144550a418a05
neil dela cruz <genoznightmare@gmail.com>
2022-10-25 15:36:50 +0800

    extracted result.hits to render for recipe

--- src/components/SearchResults.jsx
+++ src/components/SearchResults.jsx
@@ -2,7 +2,20 @@ import React, { useState, useReducer, useEffect } from "react";
 
 function SearchResults({ results }) {
   console.log("SEARCH RESULTS------------");
-  console.log(results, results.count);
+
+  if (results.hits === undefined || results.hits === []) {
+    return;
+  }
+
+  const recipes = [];
+  for (let [key, value] of Object.entries(results.hits)) {
+    recipes.push(key, value);
+  }
+
+  recipes.map((recipe) => {
+    console.log(recipe.recipe);
+  });
+
   return <div className="container mt-20"></div>;
 }
 
